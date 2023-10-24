d196160cd59ed2ee98c5d92e96d90b11d296d991
ichi <e215402@ie.u-ryukyu.ac.jp>
2023-10-24 16:34:21 +0900

    Initial Commit

---
+++ BarrierVison/BarrierVison-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  BarrierVison-Bridging-Header.h
+//  BarrierVison
+//
+//  Created by e215402 on 2023/10/24.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
