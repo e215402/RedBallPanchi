82286480ceed819b323d83923f2e9e0dd406ef0e
ichi <e215402@ie.u-ryukyu.ac.jp>
2023-12-23 21:59:24 +0900

    :sparkles: add Recording func

--- BarrierVison/ViewController.swift
+++ BarrierVison/ViewController.swift
@@ -1,8 +1,11 @@
 import UIKit
+import ReplayKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate ,RPPreviewViewControllerDelegate{
     
+    let recorder = RPScreenRecorder.shared()
+       
     @IBOutlet var sceneView: ARSCNView!
     
     @IBOutlet weak var lightON: UILabel!
@@ -13,10 +16,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     let cameraLight = UseCameraLight()
     var isTorchOn = false // トーチの状態を追跡する変数
     var isRecordOn = false //Recの状態を追跡する変数
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         guard let sceneView = sceneView else { return }
         sceneView.delegate = self
         
@@ -26,11 +29,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
-
+        
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         sceneView.addGestureRecognizer(tapGesture)
     }
-
+    
     @objc func handleTap(gestureRecognize: UITapGestureRecognizer) {
         let location = gestureRecognize.location(in: sceneView)
         let hitResults = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
@@ -39,7 +42,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             arObjectManager.placeObjects(at: position, in: sceneView.scene)
         }
     }
-                                                                                        
+    
     @IBAction func toggleLightButtonPressed(_ sender: UIButton) {
         isTorchOn.toggle() // トーチの状態を切り替える
         cameraLight.toggleTorch(on: isTorchOn)
@@ -49,23 +52,63 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             lightON.backgroundColor = UIColor.clear
         }
     }
-    
+    @objc func startRecording() {
+            recorder.startRecording { (error) in
+                if let error = error {
+                    print(error)
+            }
+        }
+    }
+
+    @objc func endRecording() {
+        recorder.stopRecording { (previewVC, error) in
+            if let previewVC = previewVC {
+                previewVC.previewControllerDelegate = self
+                self.present(previewVC, animated: true)
+            }
+            
+            if let error = error {
+                print(error)
+            }
+        }
+    }
     @IBAction func recordButtonTap(_ sender: Any) {
         isRecordOn = !isRecordOn
         if isRecordOn {
             UIView.animate(withDuration: 0.2) {
-                self.recordButton.frame = CGRect(x:182,y:705,width:30,height:30)
+                self.recordButton.frame = CGRect(x:182, y:705, width:30, height:30)
                 self.recordButton.layer.cornerRadius = 3.0
             }
+            startRecording()
+//            { error in
+//                if let error = error {
+//                    print("Error starting recording: \(error.localizedDescription)")
+//                }
+//            }
         } else {
             UIView.animate(withDuration: 0.2) {
-                self.recordButton.frame = CGRect(x:172,y:695,width:50,height:50)
+                self.recordButton.frame = CGRect(x:172, y:695, width:50, height:50)
                 self.recordButton.layer.cornerRadius = 25
             }
+            endRecording()
+            //{ error in
+            //  if let error = error {
+            //    print("Error stopping recording: \(error.localizedDescription)")
+            //}
+            }
         }
+    // RPPreviewViewControllerDelegateのメソッド
+    func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+        dismiss(animated: true, completion: nil)
     }
-    
-    
 }
+
+
+
+
+
+
+    
+
 
 
